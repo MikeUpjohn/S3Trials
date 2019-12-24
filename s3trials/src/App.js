2826933fb5ec23a942748fe43c18e0436a60d5b2
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2019-12-24 07:43:54 +0000

    S3 Trials updates.

---
+++ s3trials/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FileUpload from './Components/FileUpload/index';
+
+function App() {
+  return (
+    <div className="App">
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
---
+++ s3trials/src/Components/FileUpload/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FileUpload = () => {
+
+    return (
+        <div>
+            <h1>Heading</h1>
+        </div>
+    )
+
+};
+
+export default FileUpload;
